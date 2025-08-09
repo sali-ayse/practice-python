ad987a414185773ae9403fa74973e864d4230c94
sali-ayse <aysesali34@gmail.com>
2025-08-09 20:13:52 +0200

    task 1 in exercise 5

--- 5-list-overlap/exercise5.py
+++ 5-list-overlap/exercise5.py
@@ -12,3 +12,18 @@ Extras:
    1. Randomly generate two lists to test this
    2. Write this in one line of Python
 """
+
+import random
+
+# Hard-coded lists from example
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+c = []
+
+for i in a:
+    if i in b and i not in c:  # Make sure existing elements from list A is not in list B
+        c.append(i)
+print("new list", c)
+
+# 1. Randomly generate two lists to test this
