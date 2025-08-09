f0643725fbc8cda4e69b994659e255e6ead52daa
sali-ayse <aysesali34@gmail.com>
2025-08-09 21:39:45 +0200

    Exercise 5 - fin

--- 5-list-overlap/exercise5.py
+++ 5-list-overlap/exercise5.py
@@ -24,6 +24,24 @@ c = []
 for i in a:
     if i in b and i not in c:  # Make sure existing elements from list A is not in list B
         c.append(i)
-print("new list", c)
+print("New list", c)
 
-# 1. Randomly generate two lists to test this
+# 1. Randomly generate two lists to test this (Extra 1):
+
+
+def randomize_lists(size, start=1, end=50):
+    return random.sample(range(start, end), size)
+
+
+def find_common_elements(list_a, list_b):
+    return list(set(list_a) & set(list_b))  # Sets => only unique values, O(1)
+
+
+list_a = randomize_lists(10)
+list_b = randomize_lists(11)
+print(f"List 1 (random): {list_a}")
+print(f"List 2 (random): {list_b}")
+print(f"Common (random): {find_common_elements(list_a, list_b)}")
+
+# 2. Write this in one line of Python
+print(f"One liner, common elements: {list(set(a) & set(b))}")
